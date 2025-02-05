8a3e9a6bf84f65fe97858a134f2e2cb4bb45c464
joevaas <133934323+joevaas@users.noreply.github.com>
2025-02-05 20:03:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ DB_NAME = os.environ.get("DB_NAME", "Khaja_wazeer")
 
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002384358150"))
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001874900906"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 
 
 FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "1080")) # auto delete in seconds
